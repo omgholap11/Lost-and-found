6f4617f1e95c5f764918c68c95f834a7a6b04cf5
Om Gholap <omgholap051@gmail.com>
2026-01-13 23:12:40 +0530

    Deployment Fix

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,8 +1,5 @@
 const config = {
-  API_URL:
-    process.env.NODE_ENV === "production"
-      ? ""
-      : "http://localhost:5000",
+  API_URL: process.env.REACT_APP_API_URL || "http://localhost:5000",
 };
 
 export default config;
